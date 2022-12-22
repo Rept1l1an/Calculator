39c37a9e476c15c54cb18fe574b3cc95980cf5eb
Yusifov Telman <112288286+Rept1l1an@users.noreply.github.com>
2022-12-22 17:06:59 +0400

    Calculator
    Just a simple calculator

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq.Expressions;
+int num1,num2,num3;
+Console.WriteLine("Ededleri daxil edin:");
+num1 = int.Parse(Console.ReadLine());
+num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Emel novu secin");
+string op = Console.ReadLine();
+switch (op)
+{
+    case "+":
+        Console.WriteLine("Toplama əməlini seçdiniz:");
+        num3=num1 + num2;
+        Console.WriteLine(num3);
+        break;
+    case "-":
+        Console.WriteLine("Çıxma əməlini seçdiniz:");
+        num3=num1 - num2;
+        Console.WriteLine(num3);
+        break;
+    case "*":
+        Console.WriteLine("Vurma əməlini seçdiniz:");
+        num3=num1 * num2;
+        Console.WriteLine(num3);
+        break;
+    case "/":
+        Console.WriteLine("Bölmə əməlini seçdiniz:");
+        num3= num1 / num2;
+        Console.WriteLine(num3);
+        break;
+    case "%":
+        Console.WriteLine("Qalığı seçdiniz:");
+        num3= num1 % num2;
+        Console.WriteLine(num3);
+        break;
+    default : Console.WriteLine("Are you serious ?");
+        break;
+}
